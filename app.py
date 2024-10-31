77cb0d36b9c112d5e18bf4cab5c22a5d167e5ca9
Yuri Nunes <yuri.cruz10@hotmail.com>
2024-10-31 15:20:59 -0300

    Versão com interface

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support import expected_conditions as condicao_esperada
 from selenium.webdriver.common.keys import Keys
 from time import sleep 
 import os
-import pyautogui
 from random import randint
 
 def iniciar_driver():
@@ -37,79 +36,80 @@ def iniciar_driver():
     )
     return driver, wait
 
-driver, wait = iniciar_driver()
-# Abrir o site do linkedin
+def automatizar_convites_linkedin(cargo, mensagem):
 
-driver.get('https://www.linkedin.com/login/pt?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+    driver, wait = iniciar_driver()
+    # Abrir o site do linkedin
 
-# Realizar login (Pausa pro usuário fazer o login)
-pyautogui.alert(text='Entre com seu e-mail e senha. Logo após aguarde a automação iniciar.')
-sleep(30)
+    driver.get('https://www.linkedin.com/login/pt?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+    print('Faça o login, a automação iniciará em instantes.')
+    sleep(50)
 
-# Encontrar e clicar campo de pesquisa
-campo_pesquisar = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//input[@placeholder='Pesquisar']")))
-campo_pesquisar.click()
-sleep((randint(1,4)))
+    # Encontrar e clicar campo de pesquisa
+    campo_pesquisar = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//input[@placeholder='Pesquisar']")))
+    campo_pesquisar.click()
+    sleep((randint(1,4)))
 
-# Input pro usuário colocar qual profissão e digitar enter
-campo_pesquisar.send_keys('Desenvolvedor Python')
-sleep((randint(1,3)))
-campo_pesquisar.send_keys(Keys.ENTER)
-sleep((randint(10,15)))
+    # Input pro usuário colocar qual profissão e digitar enter
+    campo_pesquisar.send_keys(cargo)
+    sleep((randint(1,3)))
+    campo_pesquisar.send_keys(Keys.ENTER)
+    sleep((randint(10,15)))
 
-# Marcar no campo 'Pessoas'
-campo_pessoas = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//nav//div//ul//li[3]//button')))
-campo_pessoas.click()
-sleep((randint(4,8)))
+    # Marcar no campo 'Pessoas'
+    campo_pessoas = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//nav//div//ul//li[3]//button')))
+    campo_pessoas.click()
+    sleep((randint(4,8)))
 
-existe_proxima_pagina = True
+    existe_proxima_pagina = True
 
-while existe_proxima_pagina == True:
-    # Descer e subir a página
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    sleep(2)
-    driver.execute_script("window.scrollTo(0, document.body.scrollTop);")
-    sleep(2)
+    while existe_proxima_pagina == True:
+        # Descer e subir a página
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(2)
+        driver.execute_script("window.scrollTo(0, document.body.scrollTop);")
+        sleep(2)
 
-    # Verificar se tem botões 'Conectar'?
-    botões_conectar = wait.until(condicao_esperada.visibility_of_all_elements_located((By.XPATH,"//button//span[text()='Conectar']")))
-    for botao in botões_conectar:
-       
-        # Clicar no 1° campo 'Conectar'
-        botao.click()
-        sleep(randint(5,10))
+        # Verificar se tem botões 'Conectar'?
+        botões_conectar = wait.until(condicao_esperada.visibility_of_all_elements_located((By.XPATH,"//button//span[text()='Conectar']")))
+        for botao in botões_conectar:
+        
+            # Clicar no 1° campo 'Conectar'
+            botao.click()
+            sleep(randint(5,10))
 
-        # Extrair o nome
-        nome_contato = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//p//span//strong'))).text
-        primeiro_nome = nome_contato.split()[0]
-        sleep((randint(1,3)))
+            # Extrair o nome
+            nome_contato = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//p//span//strong'))).text
+            primeiro_nome = nome_contato.split()[0]
+            sleep((randint(1,3)))
 
-        # Encontrar e clicar no campo adicionar nota
-        botao_adicionar_nota = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//button[@aria-label='Adicionar nota']")))
-        botao_adicionar_nota.click()
-        sleep((randint(1,3)))
+            # Encontrar e clicar no campo adicionar nota
+            botao_adicionar_nota = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//button[@aria-label='Adicionar nota']")))
+            botao_adicionar_nota.click()
+            sleep((randint(1,3)))
 
-        # Encontrar e digitar a mensagem personalizada com o nome extraido
-        mensagem_personalizada = f'Olá, {primeiro_nome}!{os.linesep}Espero que esteja bem, notei que compartilhamos interesses em comum, estou buscando expandir minha rede com profissionais da minha área.{os.linesep}Fico a disposição para conversarmos!'
+            # Encontrar e digitar a mensagem personalizada com o nome extraido
+            mensagem_personalizada = f'Oii, {primeiro_nome}!{os.linesep}{mensagem}'
 
-        campo_mensagem = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//textarea[@name='message']")))
-        campo_mensagem.click()
-        campo_mensagem.send_keys(mensagem_personalizada)
-        sleep((randint(1,5)))
+            campo_mensagem = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,"//textarea[@name='message']")))
+            campo_mensagem.click()
+            campo_mensagem.send_keys(mensagem_personalizada)
+            sleep((randint(1,5)))
 
-        # Encontrar botão 'Enviar' e clicar
-        botao_enviar_convite = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//button[@aria-label="Enviar convite"]')))
-        botao_enviar_convite.click()
-        sleep((randint(2,7)))
+            # Encontrar botão 'Enviar' e clicar
+            botao_enviar_convite = wait.until(condicao_esperada.visibility_of_element_located((By.XPATH,'//button[@aria-label="Enviar convite"]')))
+            botao_enviar_convite.click()
+            sleep((randint(2,7)))
 
-    # Verificar se o botão avançar está ativo
-    try:
-        botao_avancar = wait.until(condicao_esperada.element_to_be_clickable((By.XPATH,'//button[@aria-label="Avançar"]')))
-        botao_avancar.click()
+        # Verificar se o botão avançar está ativo
+        try:
+            botao_avancar = wait.until(condicao_esperada.element_to_be_clickable((By.XPATH,'//button[@aria-label="Avançar"]')))
+            botao_avancar.click()
+            sleep((randint(1,5)))
 
-    except Exception as error:
-        pyautogui.alert(text='Acabou as pesquisas para Desenvolvedor Python',title='Alerta')
-        existe_proxima_pagina = False
-    
+        except Exception as error:
+            print(f'Acabou as pesquisas para {cargo}.')
+            existe_proxima_pagina = False
+        
 
-           
+            
---
+++ interface.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+from threading import Thread
+from app import automatizar_convites_linkedin
+
+sg.theme('Reddit')
+
+layout = [
+[sg.Text('Pesquisar profissão:'), sg.Input(key='cargo', size=(20,0))],
+[sg.Text('Mensagem no convite:')],
+[sg.Multiline(key='mensagem', size=(48, 7))],
+[sg.Text('Avisos:')],
+[sg.Output(size=(48,5))],
+[sg.Button('Enviar', key='botao_enviar')],
+]
+
+window = sg.Window('LinkedIn', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'botao_enviar':
+        cargo = values['cargo']
+        mensagem = values['mensagem']
+        thread_linkedin = Thread(target=automatizar_convites_linkedin, args=(cargo, mensagem), daemon=True)
+        thread_linkedin.start()
+
+        window['cargo'].update(disabled=True)
+        window['mensagem'].update(disabled=True)
+        window['botao_enviar'].update(disabled=True)
